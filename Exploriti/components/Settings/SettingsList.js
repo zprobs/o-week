1a69e00fd9d26ab482835797a26e24d1182a6897
Shayan <shayansnezhad@gmail.com>
2020-05-13 14:04:40 -0400

    General setting tab formating

--- Exploriti/components/Settings/SettingsList.js
+++ Exploriti/components/Settings/SettingsList.js
@@ -1,6 +1,10 @@
 import React, {useState} from "react";
 import { Text, View, Button, Switch, StyleSheet} from "react-native";
 import {SettingToggle} from "../ReusableComponents/SettingToggle.js";
+import Fonts from '../../theme/Fonts';
+import {Theme} from '../../theme/Colours';
+import {ThemeStatic} from '../../theme/Colours';
+import Icon from "react-native-vector-icons/EvilIcons";
 
 /**
  * Dashboard is the main view where the user can see what is important and what they need to know for the near future
@@ -13,21 +17,43 @@ import {SettingToggle} from "../ReusableComponents/SettingToggle.js";
 function SettingsList() {
 
    return (
-        <View style = {styles.viewStyle}>
-        <Text> Settings Page </Text>
+        <View>
+          <SettingsItem settingName={'General'} icon={'gear'}/>
         </View>
    );
  };
 
+const SettingsItem = ({settingName, icon}) => {
+
+  return(
+    <View style = {styles().settingItemViewStyle}>
+    <Icon name={icon} size={35} />
+    <Text style={styles().settingItemTextStyle}> {settingName} </Text>
+    <View style={{borderWidth:1, borderColor: 'black', justifyContent: 'flex-start'}}>
+    <Text style={styles().settingItemChevronStyle} name='chevron-right' size={35}> arrow</Text>
+    </View>
+    </View>
+  );
+
+}
 
 
- const styles = StyleSheet.create({
+ const styles = () => StyleSheet.create({
 
-   viewStyle:{
+   settingItemViewStyle:{
      borderWidth: 1,
      borderColor: 'black',
-     flexDirection: 'column',
-     alignItems: 'stretch',
+     flexDirection: 'row',
+     alignItems: 'center',
+     justifyContent: 'flex-start',
+     left: 10,
+     top: 10,
+   },
+   settingItemTextStyle:{
+     fontSize: 18,
+   },
+   settingItemChevronStyle: {
+
    },
    viewOneStyle: {
      height: 50,
