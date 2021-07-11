8b6eb5a40da67900929073493a8ceb23d4ec1929
Zach Probst <probst.zap@gmail.com>
2021-07-11 14:10:57 -0400

    add theme fonts

--- oweek/src/theme/index.ts
+++ oweek/src/theme/index.ts
@@ -9,19 +9,51 @@ interface Palette extends ColorTheme {
   black: string;
 }
 
+interface Font {
+  bold: string;
+  regular: string;
+  light: string;
+}
+
+interface Size {
+  superHeading: number;
+  heading: number;
+  subHeading: number;
+  label: number;
+  Body: number;
+  caption: number;
+  subText: number;
+}
+
 declare module '@material-ui/styles' {
   interface DefaultTheme {
     palette: Palette;
+    font: Font;
+    size: Size;
   }
 }
 
 const getTheme = (): DefaultTheme => {
-  const prefersDarkMode: boolean = Appearance.getColorScheme() === 'dark';
+  const prefersDarkMode = Appearance.getColorScheme() === 'dark';
   return {
     palette: {
       ...(prefersDarkMode ? dark : light),
       accent: '#0088cc',
       black: 'rgba(0,0,0,1)',
+    },
+    font: {
+      bold: 'SFProDisplay-Bold',
+      regular: 'SFProDisplay-Regular',
+      light: 'SFProDisplay-Light',
+    },
+    size: {
+      superHeading: 48,
+      heading: 32,
+      subHeading: 24,
+      label: 20,
+      Body: 16,
+      caption: 14,
+      subText: 12,
     },
   };
 };
